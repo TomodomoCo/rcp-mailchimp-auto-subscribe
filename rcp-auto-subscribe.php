410a16953ffbd6dcb295666e140079f19557c157
Ian McFarlan <ian@mcfarlan.ca>
2017-08-21 18:42:13 -0400

    base functionality; auto sub user to MC list after successful registration

---
+++ rcp-auto-subscribe.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: Restrict Content Pro Auto Subscribe
+Plugin URI: http://www.vanpattenmedia.com/
+Description: Automatically add new members to other MailChimp lists upon signup
+Version: 0.0.1
+Author: Van Patten Media Inc.
+Author URI: https://www.vanpattenmedia.com/
+Contributors: chrisvanpatten, mcfarlan
+*/
+
+/**
+ * Sample usage of using a custom MailChimp `list_id`
+ *
+ * - the 'list_id' is not the same as the id in a Mailchimp URL
+ * - use `rcp_get_mailchimp_lists()` on some template side code or using `error_log`
+ * - by default the first item in the list is the fallback if no override is present
+ * - below is a sample of using a that has been fetched manually `list_id`
+ */
+// add_filter( 'rcp_tweak_auto_signup_mailchimp_list_id', function() { return '58362286b4'; } );
+
+/**
+ * Fetch the list id to subscribe to automagically; defaults to first list in MC admin
+ *
+ * @return string $list_id
+ */
+function rcp_tweaks_set_mailchimp_list_id() {
+	$lists   = rcp_get_mailchimp_lists();
+	$list_id = apply_filters( 'rcp_tweak_auto_signup_mailchimp_list_id', $lists[0]['id'] );
+
+	return $list_id;
+}
+
+/**
+ * Automatically subscribes a user to a secondary list upon account activation
+ *
+ * @param RCP_Member $member
+ *
+ * @return void
+ */
+function ag_rcp_after_registration( $member ) {
+
+	// Bail if they don't have one of these level IDs.
+	$subscription_levels_to_check = apply_filters( 'rcp_tweaks_subscription_levels_for_auto_signup', function() {
+		return array( 1, 2 );
+	} ); // test ids as fallbacks
+
+	if ( ! in_array( $member->get_subscription_id(), $subscription_levels_to_check ) ) {
+		return;
+	}
+
+	if ( ! class_exists( 'MCAPI' ) ) {
+		require ABSPATH . '/wp-content/plugins/rcp-mailchimp/mailchimp/MCAPI.class.php';
+	}
+
+	$list_id   = rcp_tweaks_set_mailchimp_list_id();
+	$creds     = get_option( 'rcp_mailchimp_settings' );
+	$mailchimp = new MCAPI( $creds['mailchimp_api'] );
+
+	try {
+		$response = $mailchimp->listSubscribe( $list_id, $member->user_email, NULL, 'hmtl', false );
+	} catch(Exception $exception) {
+		error_log( $exception );
+	}
+
+	if ( $response == true ) {
+		// error_log( 'email subscribed: ' . $member->user_email  );
+	}
+
+	if ( $response == false ) {
+		error_log( 'email could not be subscribed: ' . $member->user_email  );
+	}
+}
+add_action( 'rcp_successful_registration', 'ag_rcp_after_registration' );
